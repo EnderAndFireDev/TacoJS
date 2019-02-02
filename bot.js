c8e7671509e5e92d3a091d812da2d562d258c651
Ender <xenderpugx@gmail.com>
2019-02-02 23:02:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
-// assda
+const { Client, RichEmbed } = require('discord.js'); // Adding them sweet embeds
+const Discord = require("discord.js"); // Just making sure discord.js in our bloody code
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`[Taco] Logged into ${client.user.tag} (${client.user.id})`)
+  client.user.setPresence({ game: { name: `Discord.js test`, });
+});
